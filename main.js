2de1463506f20bee4941960e2009f4a53de31b93
menna hamed <112440501+mennaHamed@users.noreply.github.com>
2023-01-13 22:26:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img=document.getElementById('img');
+let container=document.querySelector('.container')
+
+
+function phone(X) {
+    img.src=X;
+}
+function colors(color) {
+    container.style.background=color;
+}
